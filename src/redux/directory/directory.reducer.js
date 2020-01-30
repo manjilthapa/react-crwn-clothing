d048ff018253094c96f7b38a674835f5d577b79f
manjil thapa <manjilthapa@manjils-MacBook-Pro.local>
2020-01-30 15:35:30 +0100

    data normalization and data flow of our app

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -4,34 +4,34 @@ const INITIAL_STATE = {
             title: 'hats',
             imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
             id: 1,
-            linkUrl: 'hats'
+            linkUrl: 'shop/hats'
 
         },{
             title: 'jackets',
             imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
             id: 2,
-            linkUrl: ''
+            linkUrl: 'shop/jackets'
 
         },
         {
             title: 'sneakers',
             imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
             id: 3,
-            linkUrl: ''
+            linkUrl: 'shop/sneakers'
         },
         {
-            title: 'Women',
+            title: 'Womens',
             imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
             id: 4,
             size: 'large',
-            linkUrl: ''
+            linkUrl: 'shop/womens'
         },
         {
-            title: 'Men',
+            title: 'Mens',
             imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
             id: 5,
             size: 'large',
-            linkUrl: ''
+            linkUrl: 'shop/mens'
 
         },    
     ]
